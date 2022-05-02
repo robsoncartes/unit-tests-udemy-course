5d0aaef27fc23cf4ccf61cde5712f75002681637
Robson Sousa <robsoncartes@outlook.com>
2022-05-02 03:36:18 -0300

    'feature/session-2': class 12 - Tests execution ordination.

---
+++ src/test/java/br/com/releasesolutions/services/TestOrdination.java
@@ -0,0 +1,23 @@
+package br.com.releasesolutions.services;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestOrdination {
+
+    public static int counter = 0;
+
+    @Test
+    public void start() {
+        counter = 1;
+    }
+
+    @Test
+    public void verify() {
+        assertEquals(1, counter);
+    }
+}
